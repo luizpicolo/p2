fe4a68e14583ed41c657e1cd207044409ab08db8
Luiz Picolo <luizpicolo@gmail.com>
2018-12-12 22:16:10 -0300

    Corrigido DataTableToPolyData

--- DataTrableToPolyDataFilter.cpp
+++ DataTrableToPolyDataFilter.cpp
@@ -1,48 +1,42 @@
-#include "PointSetToPolyDataFilter.h"
+#include "DataTableToPolyDataFilter.h"
 
 namespace cg
-{ // begin namespace cg
-
-namespace vis
-{ // begin namespace vis
-
-
-void
-PointSetToPolyDataFilter::execute()
 {
-  auto output = new PolyData;
-	auto input = this->input();
-	auto points = input->getPoints();
+    namespace vis
+    {
+        void DataTableToPolyDataFilter::execute()
+        {
+          auto output = new PolyData;
+            auto input = this->input();
+            auto points = input->getData();
 
-	if (input->getDimension() > 2) {
-		for (size_t i = 0; i < points.rows(); i++)
-		{
-			VectorXf p = points.row(i);
-			output->addPoint(vec3f{ p(0), p(1), p(2) });
-		}
-	}
-	else if (input->getDimension() == 2) {
-		for (size_t i = 0; i < points.rows(); i++)
-		{
-			VectorXf p = points.row(i);
-			output->addPoint(vec3f{ p(0), p(1) });
-		}
-	}
-	else if (input->getDimension() == 1) {
-		for (size_t i = 0; i < points.rows(); i++)
-		{
-			VectorXf p = points.row(i);
-			output->addPoint(vec3f{ p(0), 0 });
-		}
-	}
-	else {
-		puts("BAD DIMENSION POINTTOPOLYDATA FILTER");
-	}
+            if (input->getDimension() > 2) {
+                for (size_t i = 0; i < points.rows(); i++)
+                {
+                    Eigen::VectorXf p = points.row(i);
+                    output->addPoint(vec3f{ p(0), p(1), p(2) });
+                }
+            }
+            else if (input->getDimension() == 2) {
+                for (size_t i = 0; i < points.rows(); i++)
+                {
+                    Eigen::VectorXf p = points.row(i);
+                    output->addPoint(vec3f{ p(0), p(1) });
+                }
+            }
+            else if (input->getDimension() == 1) {
+                for (size_t i = 0; i < points.rows(); i++)
+                {
+                    Eigen::VectorXf p = points.row(i);
+                    output->addPoint(vec3f{ p(0), 0 });
+                }
+            }
+            else {
+                puts("BAD DIMENSION FILTER");
+            }
 
-  this->setOutput(output);
-  puts("GRID2 TO POLY DATA FILTER EXECUTED");
+          this->setOutput(output);
+          puts("GRID2 TO POLY DATA FILTER EXECUTED");
+        }
+    }
 }
-
-} // end namespace vis
-
-} // end namespace cg
